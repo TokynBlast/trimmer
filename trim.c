7b1dc3e7904804a1dc63ec4beab1850997194c9d
TokynBlast <tokynblast@gmail.com>
2026-02-21 10:29:14 -0800

    Remove . prefix

--- trim.c
+++ trim.c
@@ -48,72 +48,72 @@ static alw_inl int get_ext_confidence(const char *n) {
 
   // HIGH CONFIDENCE: Source Code, Scripts, and Markup
   const char *code[] = {
-    ".c", ".h", ".cpp", ".hpp", ".cs", ".java", ".py", ".rb", ".go", ".rs",
-    ".js", ".ts", ".php", ".sh", ".bat", ".pl", ".lua", ".asm", ".s", ".ada",
-    ".ads", ".adb", ".bas", ".pas", ".sql", ".html", ".css", ".md", ".json",
-    ".xml", ".yaml", ".gd", ".gml", ".vmf", ".vmt", ".r", ".scala", ".raku",
-    ".nqp", ".perl", ".dart", ".hs", ".lisp", ".ml", ".clj", ".p", ".y", ".l"
+    "c", "h", "cpp", "hpp", "cs", "java", "py", "rb", "go", "rs",
+    "js", "ts", "php", "sh", "bat", "pl", "lua", "asm", "s", "ada",
+    "ads", "adb", "bas", "pas", "sql", "html", "css", "md", "json",
+    "xml", "yaml", "gd", "gml", "vmf", "vmt", "r", "scala", "raku",
+    "nqp", "perl", "dart", "hs", "lisp", "ml", "clj", "p", "y", "l"
   };
   for(size_t i=0; i < sizeof(code)/sizeof(char*); i++)
-    if(strcasecmp(d, code[i]) == 0) return 60;
+    if(strcasecmp(d+1, code[i]) == 0) return 60;
 
   // MEDIUM CONFIDENCE: Data Tables, Subtitles, and Configs
   const char *data[] = {
-    ".csv", ".tsv", ".srt", ".ass", ".ssa", ".ini", ".cfg", ".conf", ".txt",
-    ".log", ".reg", ".info", ".nfo", ".m3u", ".xspf", ".jsonld", ".markdown"
+    "csv", "tsv", "srt", "ass", "ssa", "ini", "cfg", "conf", "txt",
+    "log", "reg", "info", "nfo", "m3u", "xspf", "jsonld", "markdown"
   };
   for(size_t i=0; i < sizeof(data)/sizeof(char*); i++)
-    if(strcasecmp(d, data[i]) == 0) return 40;
+    if(strcasecmp(d+1, data[i]) == 0) return 40;
 
   // NEGATIVE CONFIDENCE (BINARY PENALTY): Media, Game ROMs, and Executables
   const char *binary[] = {
     // Executables and Libraries
-    ".exe", ".dll", ".so", ".bin", ".com", ".o", ".a", ".lib", ".ko", ".elf",
-    ".class", ".jar", ".war", ".ear", ".deb", ".rpm", ".msi", ".apk", ".ipa",
-    ".app", ".dmg", ".pkg", ".xpi", ".appx", ".msix", ".dol", ".xbe", ".xex",
+    "exe", "dll", "so", "bin", "com", "o", "a", "lib", "ko", "elf",
+    "class", "jar", "war", "ear", "deb", "rpm", "msi", "apk", "ipa",
+    "app", "dmg", "pkg", "xpi", "appx", "msix", "dol", "xbe", "xex",
     // Archives and Compressed
-    ".zip", ".7z", ".rar", ".gz", ".bz2", ".xz", ".lz", ".lzma", ".lzo",
-    ".tar", ".tgz", ".tbz2", ".txz", ".cab", ".arj", ".ace", ".arc", ".alz",
-    ".lzh", ".zoo", ".z", ".zst", ".br", ".sitx", ".sea", ".cpt", ".egg",
+    "zip", "7z", "rar", "gz", "bz2", "xz", "lz", "lzma", "lzo",
+    "tar", "tgz", "tbz2", "txz", "cab", "arj", "ace", "arc", "alz",
+    "lzh", "zoo", "z", "zst", "br", "sitx", "sea", "cpt", "egg",
     // Disk Images and Virtual Disks
-    ".iso", ".img", ".vhd", ".vhdx", ".vmdk", ".vdi", ".dmg", ".nrg", ".mds",
-    ".mdx", ".cue", ".cdi", ".cif", ".daa", ".gho", ".ghs", ".tib", ".wim",
-    ".swm", ".esd", ".adf", ".adz", ".d64", ".dms", ".dsk", ".qcow", ".qcow2",
+    "iso", "img", "vhd", "vhdx", "vmdk", "vdi", "dmg", "nrg", "mds",
+    "mdx", "cue", "cdi", "cif", "daa", "gho", "ghs", "tib", "wim",
+    "swm", "esd", "adf", "adz", "d64", "dms", "dsk", "qcow", "qcow2",
     // Audio
-    ".mp3", ".wav", ".flac", ".aac", ".ogg", ".oga", ".opus", ".m4a", ".wma",
-    ".aiff", ".aif", ".ape", ".mpc", ".wv", ".tta", ".tak", ".ac3", ".dts",
-    ".mid", ".midi", ".mod", ".xm", ".it", ".s3m", ".sid", ".spc", ".nsf",
+    "mp3", "wav", "flac", "aac", "ogg", "oga", "opus", "m4a", "wma",
+    "aiff", "aif", "ape", "mpc", "wv", "tta", "tak", "ac3", "dts",
+    "mid", "midi", "mod", "xm", "it", "s3m", "sid", "spc", "nsf",
     // Video
-    ".mp4", ".mkv", ".avi", ".mov", ".wmv", ".flv", ".webm", ".m4v", ".mpg",
-    ".mpeg", ".m2v", ".vob", ".3gp", ".3g2", ".mxf", ".rm", ".rmvb", ".asf",
-    ".ogv", ".ts", ".m2ts", ".mts", ".divx", ".xvid", ".bik", ".bk2", ".roq",
-    ".smk", ".thp", ".dvr-ms", ".wtv", ".swf",
+    "mp4", "mkv", "avi", "mov", "wmv", "flv", "webm", "m4v", "mpg",
+    "mpeg", "m2v", "vob", "3gp", "3g2", "mxf", "rm", "rmvb", "asf",
+    "ogv", "ts", "m2ts", "mts", "divx", "xvid", "bik", "bk2", "roq",
+    "smk", "thp", "dvr-ms", "wtv", "swf",
     // Images
-    ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff", ".tif", ".webp", ".avif",
-    ".heic", ".heif", ".ico", ".icns", ".psd", ".xcf", ".raw", ".cr2", ".nef",
-    ".dng", ".arw", ".orf", ".rw2", ".pef", ".sr2", ".tga", ".dds", ".hdr",
-    ".exr", ".jp2", ".jxl", ".pcx", ".pbm", ".pgm", ".ppm", ".svg", ".svgz",
+    "png", "jpg", "jpeg", "gif", "bmp", "tiff", "tif", "webp", "avif",
+    "heic", "heif", "ico", "icns", "psd", "xcf", "raw", "cr2", "nef",
+    "dng", "arw", "orf", "rw2", "pef", "sr2", "tga", "dds", "hdr",
+    "exr", "jp2", "jxl", "pcx", "pbm", "pgm", "ppm", "svg", "svgz",
     // Fonts
-    ".ttf", ".otf", ".woff", ".woff2", ".eot", ".fon", ".fnt", ".pfb", ".pfm",
+    "ttf", "otf", "woff", "woff2", "eot", "fon", "fnt", "pfb", "pfm",
     // Game ROMs and Data
-    ".nes", ".snes", ".gba", ".gbc", ".gb", ".nds", ".3ds", ".n64", ".z64",
-    ".v64", ".sfc", ".smc", ".md", ".smd", ".gg", ".pce", ".vb", ".ws", ".wsc",
-    ".gcm", ".wbfs", ".rvz", ".nsp", ".xci", ".cia", ".vpk", ".pbp", ".cso",
-    ".wad", ".pak", ".pk2", ".pk3", ".pk4", ".bsp", ".gcf", ".vpk", ".mpq",
-    ".pbo", ".upk", ".uasset", ".umap", ".u", ".mdl", ".vtf", ".vmt", ".bsp",
+    "nes", "snes", "gba", "gbc", "gb", "nds", "3ds", "n64", "z64",
+    "v64", "sfc", "smc", "md", "smd", "gg", "pce", "vb", "ws", "wsc",
+    "gcm", "wbfs", "rvz", "nsp", "xci", "cia", "vpk", "pbp", "cso",
+    "wad", "pak", "pk2", "pk3", "pk4", "bsp", "gcf", "vpk", "mpq",
+    "pbo", "upk", "uasset", "umap", "u", "mdl", "vtf", "vmt", "bsp",
     // Databases
-    ".db", ".sqlite", ".sqlite3", ".mdb", ".accdb", ".fdb", ".gdb", ".dbf",
+    "db", "sqlite", "sqlite3", "mdb", "accdb", "fdb", "gdb", "dbf",
     // Documents (binary)
-    ".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".odt", ".ods",
-    ".odp", ".epub", ".mobi", ".azw", ".azw3",
+    "pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "odt", "ods",
+    "odp", "epub", "mobi", "azw", "azw3",
     // 3D Models
-    ".obj", ".fbx", ".dae", ".3ds", ".blend", ".max", ".mb", ".ma", ".c4d",
-    ".stl", ".ply", ".gltf", ".glb", ".usd", ".usda", ".usdc", ".usdz",
+    "obj", "fbx", "dae", "3ds", "blend", "max", "mb", "ma", "c4d",
+    "stl", "ply", "gltf", "glb", "usd", "usda", "usdc", "usdz",
     // Miscellaneous Binary
-    ".pyc", ".pyo", ".elc", ".beam", ".rlib", ".pdb", ".dmp", ".core"
+    "pyc", "pyo", "elc", "beam", "rlib", "pdb", "dmp", "core"
   };
   for(size_t i=0; i < sizeof(binary)/sizeof(char*); i++)
-    if(strcasecmp(d, binary[i]) == 0) return -100;
+    if(strcasecmp(d+1, binary[i]) == 0) return -100;
 
   return 5;
 }
